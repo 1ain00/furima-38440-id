1f3e5da47c6a66221e3d25ad61fc38fe93b6eaf8
1ain00 <ponpon.krr-n@i.softbank.jp>
2022-09-15 14:45:42 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   end
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
   # has_many :items
   # has_many :buys
