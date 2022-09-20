524b811833529690491e5293afbb09ad5ed113ed
1ain00 <ponpon.krr-n@i.softbank.jp>
2022-09-20 20:00:49 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -27,13 +27,18 @@ class ItemsController < ApplicationController
   end
 
   def update
-   
     if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
     end
   end
+
+def destroy
+  @item.destroy
+  redirect_to root_path
+end
+
 
   private
 
